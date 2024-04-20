d2c0034325f816f96afb19d39edb750f24f8e6c7
Nilav Prajapati <nilavprajapati2004@gmail.com>
2024-04-20 15:54:18 +0530

    removed comments

--- sketch-stylus/src/constants.rs
+++ sketch-stylus/src/constants.rs
@@ -7,43 +7,6 @@ pub struct Constants;
 
 impl ConstantParams for Constants {
     fn training_data() -> (Vec<Vec<i128>>, Vec<Vec<i128>>) {
-        // let file_path = "./../csv/train.csv";
-        // let file = File::open(file_path).unwrap();
-        // let mut rdr = Reader::from_reader(file);
-        // let mut records = Vec::new();
-        // for result in rdr.records() {
-        //     let record = result.unwrap();
-        //     records.push(record);
-        // }
-
-        // // let mut rng = rand::thread_rng();
-        // // records.shuffle(&mut rng);
-
-        // let _ = records.len();
-        // let n = recordsvec!vec![0].len();
-        // let mut x_train = Vec::new();
-        // let mut y_temp = Vec::new();
-
-        // for record in records {
-        //     let mut x = Vec::new();
-        //     for i in 1..n {
-        //         let mut value: f32 = recordvec!vec![i].parse().unwrap();
-        //         //changing here
-        //         value /= 255;
-        //         value *= 1000;
-
-        //         x.push(value as i128); // Assuming normalization by 255 as in your Python code
-        //     }
-        //     x_train.push(x);
-        //     let mut y: f32 = recordvec!vec![0].parse().unwrap();
-        //     // changing the scale
-        //     y *= 1000;
-        //     y_temp.push(y as i128);
-        // }
-        // let y_train = vec!vec![y_temp];
-
-        // let x_train = vec![vec![0; 784]; 41000];
-        // let y_train = vec![vec![0; 785]; 1];
         let y_train = vec![vec![
             5000, 5000, 9000, 1000, 2000, 7000, 3000, 2000, 7000, 4000,
         ]];
